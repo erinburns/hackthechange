17bcc09b39ce815447d8e67f27aa585381a0faf5
Justin Grierson <Justin Grierson>
2016-11-12 14:04:47 +0000

    first commit

--- index.php
+++ index.php
@@ -6,6 +6,22 @@ $conn_string = "host=ec2-54-75-232-56.eu-west-1.compute.amazonaws.com port=5432 
 $dbconn4 = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
 ?>
 
 
