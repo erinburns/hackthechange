1ad9aa3a5c979373728e2b423869120604e8452a
Justin Grierson <Justin Grierson>
2016-11-12 13:43:40 +0000

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
+<?php
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+); ?>
 
 <button onclick="openWin()">Open w3schools.com in a new window</button>
 <button onclick="closeWin()">Close the new window (w3schools.com)</button>
