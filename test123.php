2d3527317fbbcfa12d6094f89c2056a943794068
Justin Grierson <Justin Grierson>
2016-11-12 20:05:36 +0000

    update connection page

---
+++ test123.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "enablers";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE reports (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+message text,
+attachmenturl text,
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
