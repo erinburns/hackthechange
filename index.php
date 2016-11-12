48138958dc935c0127463b5b119c6bf6a28ead24
Justin Grierson <Justin Grierson>
2016-11-12 13:59:13 +0000

    first commit

--- index.php
+++ index.php
@@ -14,8 +14,6 @@ $conn_string = "host=sheep port=5432 dbname=test user=lamb password=bar";
 $dbconn4 = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
 
-$dbconn5 = pg_connect("host=localhost options='--client_encoding=UTF8'");
-//connect to a database on "localhost" and set the command line parameter which tells the encoding is in UTF-8
 ?>
 
 
