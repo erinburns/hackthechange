bfe3f253f4ec39eee9d4ebb23937f160aa2cf148
Duncan MacKenzie <duncan.mackenzie@phe.gov.uk>
2016-11-13 14:40:37 +0000

    Update thankyou.php with branding

--- thankyou.php
+++ thankyou.php
@@ -1,3 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>The Enablers</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar.css" rel="stylesheet">
+    <link href="css/mystyle.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <!-- Static navbar -->
+      <nav class="navbar navbar-inverse ">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+             
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Panic<span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Escape</a></li>
+            
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+  
+  
+    <!-- Jumbotron -->
+  
+      <div class="jumbotron jumboback">
+        <h2>Thank you for your report!</h2>
+        <p class="lead">One of our helpful experts will look at it shortly.</p>
+      <br>
+        <br>
+         <br>
+        <div class="homebutton">
+        
+        <a class="btn btn-lg btn-success" href="/index.html" role="button">Back to home page</a>
+         
+         </div>
+      </div>
+  
+  
+  
+  
+
+
+    </div> <!-- /container -->
+
+
+<footer>
+</footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
@@ -12,7 +114,7 @@ $dbname = "enablers";
 $link = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($link->connect_error) {
-    die("Connection failed: " . $link->connect_error);
+    //die("Connection failed: " . $link->connect_error);
 } 
 
 // sql to create table
@@ -34,7 +136,7 @@ if ($link->query($sql) === TRUE) {
 
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    //die("ERROR: Could not connect. " . mysqli_connect_error());
 }
  
 // Escape user inputs for security
@@ -43,9 +145,9 @@ $message = mysqli_real_escape_string($link, $_POST['message']);
 // attempt insert query execution
 $sql = "INSERT INTO reports (message) VALUES ('$message')";
 if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
+//echo "Records added successfully.";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    //echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
 // close connection
